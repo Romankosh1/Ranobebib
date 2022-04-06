69ee898323168123e752cf3083138d7c37e5d4da
Roman Danylenko <danilenko.roma2016@gmail.com>
2022-04-06 09:20:58 +0300

    I forgot save file

--- src/components/Home/components/MediaCard.js
+++ src/components/Home/components/MediaCard.js
@@ -2,19 +2,24 @@ import { Link } from "react-router-dom";
 
 const MediaCard = () => {
   return (
-    <div className="card">
+    <div className="card border-light">
       <Link to="Tower">
-        <img
-          src="/img/tower.jpg"
-          className="card-img-top"
-          alt="Mightiness Tower"
-        />
-      </Link>
-      <div className="card-body">
-        <a
-          href="Tower"
-          className="text-decoration-none text-reset card-link fs-6 line-clamp"
+        <div
+          className="position-relative"
+          title="Читать 99 главу. Башня небес!"
         >
+          <img
+            src="/img/tower.jpg"
+            className="card-img-top"
+            alt="Mightiness Tower"
+          />
+          <div className="position-absolute bottom-0 start-5 text-white">
+            Том 1 Глава 99
+          </div>
+        </div>
+      </Link>
+      <div className="card-body ">
+        <a href="Tower" className="fs-6 line-clamp hover-link">
           Mightiness Tower
         </a>
       </div>
--- src/components/Home/components/PopularUpdateList.js
+++ src/components/Home/components/PopularUpdateList.js
@@ -3,7 +3,7 @@ import MediaCard from "./MediaCard";
 const PopularUpdateList = () => {
   return (
     <div className="bg-white rounded pt-2 px-3 pb-3">
-      <h5>Обновления популярного ранобэ</h5>
+      <h5 className="mb-3">Обновления популярного ранобэ</h5>
       <div className="row flex-nowrap gx-2">
         <div className="col-2">
           <MediaCard />
